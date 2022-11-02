b9a8b3c7539987f726a5b0e006238117ba193e9c
안재욱 <ajw1079@naver.com>
2022-11-02 13:29:45 +0900

    mer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header-wrapper">
+    <h1 class="logo">Hello {{name}}</h1>
+    <button @click="showAlert">Click Me</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      name : "James"
+    }
+  },
+  methods : {
+    showAlert(e) {
+      alert("Hello " + this.name);
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.header-wrapper{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:red;
 }
 </style>
